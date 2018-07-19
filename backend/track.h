73ff2f6514f7e046e4ff6cb69c49f5ee59613926
Dmitry Paliy <dmitrydpaliy@gmail.com>
2018-07-19 21:45:10 +0300

    track.* files moved to backend folder

---
+++ backend/track.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/* class Track {
+    public:
+        int getEndX();
+        int getEndY();
+        static int getCount();
+        static void setCount(int);
+        static Track** array;
+        static void GenTracks();
+    private:
+        int endY;
+        int endX;
+        float angle;
+        static int count;
+        Track(int);
+        ~Track();
+//      static float countAngle(int, int);
+}; */
